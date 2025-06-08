962fbee14f27b89734b179fbd48f1a620d311d7b
Jerry Clark <jerry.cabuntucan@tuitt.com>
2025-06-08 13:19:34 +0800

    added loading page

---
+++ app/blog/[slug]/loading.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex items-center justify-center h-[500px]'>
+        <span className="loader"></span>
+    </div>
+  )
+}
+
+export default Loading
